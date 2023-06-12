72ac1cba22ed655b5b84a3654dfa831eceb56678
Skjiisa <elaine@lyons.app>
2023-06-12 17:14:45 -0600

    Initial Commit

---
+++ SwiftUI Data Flow/SwiftUI_Data_FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Data_FlowApp.swift
+//  SwiftUI Data Flow
+//
+//  Created by Elaine Lyons on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Data_FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
